bd347639223b44a977f82cc015ff710ec1fbaf09
Ravi Asthana <rantoinne@Ravis-Mac-mini.local>
2022-10-21 14:19:34 +0530

    project initial setup

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{}}>
+      <View style={{ width: 200, height: 200}}>
+        <Text>TEST</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
