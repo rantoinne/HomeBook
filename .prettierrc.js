2783e09bfedb4c655e1c8774c9f34ad7e6138459
Ravi Asthana <rantoinne@Ravis-Mac-mini.local>
2022-10-21 14:26:17 +0530

    prject configs update

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,9 @@ module.exports = {
   bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  jsxSingleQuote: true,
+  printWidth: 120,
+  endOfLine: "auto",
+  tabWidth: 2,
+  useTabs: false,
 };
